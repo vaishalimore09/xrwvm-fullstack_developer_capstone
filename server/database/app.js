0aeb706634d9b85774acfab925c3fdf749c0010e
vaishalimore09 <vaishali.more9520@gmail.com>
2024-10-08 19:56:22 +0530

    lint update

--- server/database/app.js
+++ server/database/app.js
@@ -102,18 +102,18 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   const data = JSON.parse(req.body);
   const documents = await Reviews.find().sort({ id: -1 });
-  const new_id = documents[0]['id'] + 1;
+  const new_id = documents[0].id + 1;
 
   const review = new Reviews({
     "id": new_id,
-    "name": data['name'],
-    "dealership": data['dealership'],
-    "review": data['review'],
-    "purchase": data['purchase'],
-    "purchase_date": data['purchase_date'],
-    "car_make": data['car_make'],
-    "car_model": data['car_model'],
-    "car_year": data['car_year'],
+    "name": data.name,
+    "dealership": data.dealership,
+    "review": data.review,
+    "purchase": data.purchase,
+    "purchase_date": data.purchase_date,
+    "car_make": data.car_make,
+    "car_model": data.car_model,
+    "car_year": data.car_year,
   });
 
   try {
