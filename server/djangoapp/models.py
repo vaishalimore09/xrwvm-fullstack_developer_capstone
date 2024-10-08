a84765bbe95688f889683d4c6f33fa4cfc14bf70
vaishalimore09 <146318217+vaishalimore09@users.noreply.github.com>
2024-10-08 18:21:53 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -24,20 +23,20 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
-from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, 
+                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -47,19 +46,23 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            validators=[
+                MaxValueValidator(2023),
+                MinValueValidator(2015)
+            ])
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # All fields are from dealerships document in cloudant
 class CarDealer:
 
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+    def __init__(self, address, city, 
+                 full_name, id, 
+                 lat, long, short_name, 
+                 st, zip):
         # Dealer address
         self.address = address
         # Dealer city
@@ -88,7 +91,12 @@ class CarDealer:
 # All fields are from review documet in cloudant
 class DealerReview:
 
-    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment='neutral'):
+    def __init__(self, id, name, 
+                 dealership, 
+                 review, purchase, 
+                 purchase_date,
+                 car_make, car_model, 
+                 car_year, sentiment='neutral'):
         self.id = id
         self.name = name  # reviewer name
         self.dealership = dealership
@@ -102,3 +110,4 @@ class DealerReview:
 
         def __str__(self):
             return 'Reviewer: ' + self.name + ' Review: ' + self.review
+    
