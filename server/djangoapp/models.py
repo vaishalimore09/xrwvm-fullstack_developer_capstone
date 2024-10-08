be7eee17218c2bb59132f6c176a244c86bd79521
vaishalimore09 <146318217+vaishalimore09@users.noreply.github.com>
2024-10-08 18:26:08 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,42 +1,18 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 # Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, 
-                                 on_delete=models.CASCADE)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -45,62 +21,39 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-            validators=[
-                MaxValueValidator(2023),
-                MinValueValidator(2015)
-            ])
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
-# All fields are from dealerships document in cloudant
 class CarDealer:
-
-    def __init__(self, address, city, 
-                 full_name, id, 
-                 lat, long, short_name, 
-                 st, zip):
-        # Dealer address
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
         self.address = address
-        # Dealer city
         self.city = city
-        # Dealer Full Name
         self.full_name = full_name
-        # Dealer id
         self.id = id
-        # Location lat
         self.lat = lat
-        # Location long
         self.long = long
-        # Dealer short name
         self.short_name = short_name
-        # Dealer state
         self.st = st
-        # Dealer zip
         self.zip = zip
 
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
-
-# All fields are from review documet in cloudant
 class DealerReview:
-
-    def __init__(self, id, name, 
-                 dealership, 
-                 review, purchase, 
-                 purchase_date,
-                 car_make, car_model, 
-                 car_year, sentiment='neutral'):
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment='neutral'):
         self.id = id
         self.name = name  # reviewer name
         self.dealership = dealership
-        self.review = review  # reveiw text
+        self.review = review  # review text
         self.purchase = purchase  # Did the reviewer purchase the car? bool
         self.purchase_date = purchase_date
         self.car_make = car_make
@@ -108,6 +61,5 @@ class DealerReview:
         self.car_year = car_year
         self.sentiment = sentiment  # Watson NLU sentiment analysis of review
 
-        def __str__(self):
-            return 'Reviewer: ' + self.name + ' Review: ' + self.review
-    
+    def __str__(self):
+        return 'Reviewer: ' + self.name + ' Review: ' + self.review
